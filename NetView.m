d3cc6b5ff22c8b0836830a43e4afb87534c61ef8
pialin <765422908@qq.com>
2016-06-03 00:52:20 +0800

    add a little to net view

--- NetView.m
+++ NetView.m
@@ -3,27 +3,49 @@ classdef NetView < handle
         HandleFigure;
     end
     methods
-        function nvobj = NetView()
+        function nvobj = NetView(nmobj)
+            get(0,'ScreenSize')
             if verLessThan('matlab','R2016a')
                 
             else
-                nvobj.HandleFigure = uifigure;
-                nvobj.HandleFigure.Position = [100 100 640 480];
-                app.UIFigure.Name = 'NetViewer';
-                setAutoResize(app, app.UIFigure, true)
+                
+                nvobj.nmobj = nmobj;
+                
+                
+                nvobj.HandleUifigure.Name = 'NetView';
+                nvobj.HandleUifigure.Position = [100 100 640 480];
+                nvobj.HandleUifigure.Units = 'normalized';
+                nvobj.HandleUifigure.Resize = 'on';
+                nvobj.HandleUifigure.SizeChangedFcn = @;
+                
+                setAutoResize(nvobj, nvobj.HandleUifigure, true)
+                
+                nvobj.nmobj.addlistener('event',@fcn);
+                nvobj.BuildUi;
+                nvobj.ncobj = nvobj.CreateController();
+                nvobj.AttachToController(nvobj.ncobj);
+            end
+            
+            function BuildUi(nvobj)
+                nvobj.HandleFigure = uifigure(nvobj.HandleUifigure);
+                
+                % Create LabelSlider
+                nvobj.LabelSlider = uilabel(nvobj.HandleUifigure);
+                nvobj.LabelSlider.HorizontalAlignment = 'right';
+                nvobj.LabelSlider.Position = [181 244 33 15];
+                nvobj.LabelSlider.Text = 'Slider';
+                
+                % Create Slider
+                nvobj.Slider = uislider(nvobj.UIFigure);
+                nvobj.Slider.Position = [235 250 150 3];
+                nvobj.HandleSlider =
+                
+                
             end
             
             
             
-            % Create LabelSlider
-            app.LabelSlider = uilabel(app.UIFigure);
-            app.LabelSlider.HorizontalAlignment = 'right';
-            app.LabelSlider.Position = [181 244 33 15];
-            app.LabelSlider.Text = 'Slider';
             
-            % Create Slider
-            app.Slider = uislider(app.UIFigure);
-            app.Slider.Position = [235 250 150 3];
             
             
         end
